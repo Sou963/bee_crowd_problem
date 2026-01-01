b7d01205e61be85cfd98da6e8e1b02934783f27f
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2026-01-02 01:01:44 +0600

    Add files via upload

--- p_1101.cpp
+++ p_1101.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int M, N;
-
     while (true) {
         cin >> M >> N;
-
         if (M <= 0 || N <= 0)
             break;
-
         int start = min(M, N);
         int end = max(M, N);
-
         int sum = 0;
-
         for (int i = start; i <= end; i++) {
             cout << i << " ";
             sum += i;
         }
-
         cout << "Sum=" << sum << endl;
     }
 
